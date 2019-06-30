51d85b68b9636c50eadfbae677878f092f81422a
qubitAI <osmanmorales@ymail.com>
2019-06-30 10:32:16 -0400

    Update loopinf.py

--- loopinf.py
+++ loopinf.py
@@ -1,14 +1,15 @@
-i = 0
-b = 1
+def runLopp(ii, bb):
+  i = ii
+  b = bb
 
-for i in range(50000):
-  n = 20
-  if n % b == 0:
-    print(i)
-  else:
-    for k in range((n*i)):
-      print(k)
-  print("Printing for k")
+  for i in range(50000):
+    n = 20
+    if n % b == 0:
+      print(i)
+    else:
+      for k in range((n*i)):
+        print(k)
+    print("Printing for k")
       
     
     
