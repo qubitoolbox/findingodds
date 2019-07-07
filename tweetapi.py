fee3e41856d0f6466830aab82d1ddc6b9166195b
qubitAI <osmanmorales@ymail.com>
2019-07-07 17:48:35 -0400

    Update tweetapi.py

--- tweetapi.py
+++ tweetapi.py
@@ -5,4 +5,5 @@ from textblob import TextBlob as tb
 def apiCall(word):
   lkey = {'consumer_key':"ckey_goes_here"}
   lkey1 = {'consumer_secret':"csecr_goes_here"}
-  
+  akey = {'access_token':"keyhere"}
+  akey1 = {'access_token_secret':"keyhere"}
